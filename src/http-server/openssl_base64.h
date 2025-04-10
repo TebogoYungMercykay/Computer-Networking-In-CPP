1f27e6c298b99dedc4e97daee41eae95ab23a6aa
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2025-04-10 12:00:29 +0200

    (delete) Removed Cluttered Files

--- src/http-server/openssl_base64.h
+++
@@ -1,9 +0,0 @@
-#ifndef OPENSSL_BASE64_H
-#define OPENSSL_BASE64_H
-
-#include <string>
-
-std::string base64_encode(const std::string& input);
-std::string base64_decode(const std::string& input);
-
-#endif // OPENSSL_BASE64_H
