8094117c78b2daa30943940193cdb312251a6f45
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2025-04-21 22:04:32 +0200

    (smtp) Utility Functions For Base64 Encoding & Color Formatting

---
+++ src/smtp-server/utils.cpp
@@ -0,0 +1,94 @@
+#include "utils.h"
+#include <iostream>
+#include <unistd.h>
+#include <fcntl.h>
+
+std::string ansiColor(int color) {
+    return "\033[" + std::to_string(color) + "m";
+}
+
+std::string ansiReset() {
+    return "\033[0m";
+}
+
+std::string clearScreen() {
+    return "\033[2J\033[H";
+}
+
+// Base64 encoding function
+std::string base64_encode(const std::string& input) {
+    static const std::string base64_chars = 
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    
+    std::string encoded;
+    int i = 0;
+    unsigned char char_array_3[3];
+    unsigned char char_array_4[4];
+    int in_len = input.size();
+    
+    while (in_len--) {
+        char_array_3[i++] = input[i-1];
+        if (i == 3) {
+            char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+            char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+            char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+            char_array_4[3] = char_array_3[2] & 0x3f;
+
+            for(i = 0; i < 4; i++)
+                encoded += base64_chars[char_array_4[i]];
+            i = 0;
+        }
+    }
+
+    if (i) {
+        for(int j = i; j < 3; j++)
+            char_array_3[j] = '\0';
+
+        char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+        char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+        char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+        char_array_4[3] = char_array_3[2] & 0x3f;
+
+        for (int j = 0; j < i + 1; j++)
+            encoded += base64_chars[char_array_4[j]];
+
+        while(i++ < 3)
+            encoded += '=';
+    }
+
+    return encoded;
+}
+
+// Read character without waiting for Enter key
+char getch_nonblock() {
+    char buf = 0;
+    struct termios old = {0};
+    if (tcgetattr(0, &old) < 0)
+        perror("tcsetattr()");
+    
+    old.c_lflag &= ~ICANON;
+    old.c_lflag &= ~ECHO;
+    old.c_cc[VMIN] = 0;
+    old.c_cc[VTIME] = 0;
+    
+    if (tcsetattr(0, TCSANOW, &old) < 0)
+        perror("tcsetattr ICANON");
+    
+    if (read(0, &buf, 1) < 0)
+        perror("read()");
+    
+    old.c_lflag |= ICANON;
+    old.c_lflag |= ECHO;
+    
+    if (tcsetattr(0, TCSADRAIN, &old) < 0)
+        perror("tcsetattr ~ICANON");
+    
+    return buf;
+}
+
+// Get input with standard canonical mode
+std::string getInput() {
+    std::string input;
+    std::getline(std::cin, input);
+    return input;
+}
