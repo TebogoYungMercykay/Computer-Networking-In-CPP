51ee58391b62627e07a7ceea9f2ebe150d481f27
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2025-04-22 01:13:22 +0200

    (smtp) Utility Functions For Base64 Encoding & Color Formatting

--- src/smtp-server/utils.h
+++ src/smtp-server/utils.h
@@ -4,18 +4,14 @@
 #include <string>
 #include <termios.h>
 
-// ANSI color codes and terminal control sequences
 std::string ansiColor(int color);
 std::string ansiReset();
 std::string clearScreen();
 
-// Base64 encoding function
 std::string base64_encode(const std::string& input);
 
-// Read character without waiting for Enter key
 char getch_nonblock();
 
-// Get input with standard canonical mode
 std::string getInput();
 
 #endif // UTILS_H
