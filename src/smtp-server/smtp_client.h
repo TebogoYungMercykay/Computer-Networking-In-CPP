d7d6551cf8ec3c12c11a99a45b5d8f9f471817c8
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2025-04-22 16:59:24 +0200

    (smtp) Added Indentation for The SMTP Client Header File

--- src/smtp-server/smtp_client.h
+++ src/smtp-server/smtp_client.h
@@ -5,26 +5,26 @@
 #include <openssl/ssl.h>
 
 class SMTPClient {
-private:
-    int socket_fd;
-    SSL* ssl;
-    SSL_CTX* ctx;
-    bool use_ssl;
+    private:
+        int socket_fd;
+        SSL* ssl;
+        SSL_CTX* ctx;
+        bool use_ssl;
 
-    bool send_data(const std::string& data);
-    std::string receive_data();
-    bool check_response(int expected_code);
+        bool send_data(const std::string& data);
+        std::string receive_data();
+        bool check_response(int expected_code);
 
-public:
-    SMTPClient(bool use_ssl = true);
-    ~SMTPClient();
-    
-    bool connect_to_server(const std::string& host, int port);
-    bool authenticate(const std::string& username, const std::string& password);
-    bool send_email(const std::string& from, const std::string& to, 
-                    const std::string& subject, const std::string& body);
-    bool quit();
-    void close_connection();
+    public:
+        SMTPClient(bool use_ssl = true);
+        ~SMTPClient();
+        
+        bool connect_to_server(const std::string& host, int port);
+        bool authenticate(const std::string& username, const std::string& password);
+        bool send_email(const std::string& from, const std::string& to, 
+                        const std::string& subject, const std::string& body);
+        bool quit();
+        void close_connection();
 };
 
 #endif // SMTP_CLIENT_H
