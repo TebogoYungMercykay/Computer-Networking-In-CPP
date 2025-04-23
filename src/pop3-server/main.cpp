4c03c89c28afce5c973183ad39472165a62824f7
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2025-04-24 00:37:13 +0200

    (pop3) Removed Unused Function From Main

--- src/pop3-server/main.cpp
+++ src/pop3-server/main.cpp
@@ -5,14 +5,7 @@
 #include "utils.h"
 #include "email_manager.h"
 
-void print_usage() {
-    std::cout << "Usage: ./program [options]" << std::endl;
-    std::cout << "Options:" << std::endl;
-    std::cout << "  --email      Start the email manager" << std::endl;
-    std::cout << "  --help       Display this help message" << std::endl;
-}
-
-int main(int argc, char* argv[]) {
+int main() {
     EmailManager email_manager;
     email_manager.run();
 
