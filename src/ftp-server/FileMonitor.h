6f3a741b1b54e0bf5920ef35a87679ce3a95361b
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2025-05-20 04:34:56 +0200

    (ftp) Adding Mechanism To Terminate Program Gracefully

--- src/ftp-server/FileMonitor.h
+++ src/ftp-server/FileMonitor.h
@@ -4,21 +4,22 @@
 #include "Config.h"
 #include "FtpClient.h"
 #include <filesystem>
+#include <functional>
 
 // File monitoring class
 class FileMonitor {
     private:
         Config config;
         std::filesystem::file_time_type lastModified;
-        
-        // Public methods
-        
+        std::function<void()> handler;
+
         std::filesystem::file_time_type getFileModificationTime(const std::string& filepath);
         bool uploadFile();
         void createDirectoryStructure(FtpClient& client, const std::string& remotePath);
         void monitorWithPolling();
+
     public:
-        FileMonitor(const Config& config);
+        FileMonitor(const Config& config, std::function<void()> handler);
         void monitor();
 };
 
