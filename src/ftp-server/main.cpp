3556b6d7ebef96f1912269ed039abe9e286dc26f
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2025-05-20 03:50:10 +0200

    (ftp) The Main Program For The FTP Server

--- src/ftp-server/main.cpp
+++ src/ftp-server/main.cpp
@@ -1,9 +1,28 @@
+#include "Config.h"
+#include "FileMonitor.h"
 #include <iostream>
-#include <openssl/ssl.h>
-#include <openssl/err.h>
+#include <stdexcept>
 
-int main() {
-    std::cout << "Hello World!" << std::endl;
-    
-    return 0;
+int main(int argc, char* argv[]) {
+    try {
+        std::string envPath = ".env";
+        
+        // Allow specifying a different .env file via command line
+        if (argc > 1) {
+            envPath = argv[1];
+        }
+        
+        // Load configuration
+        Config config = loadConfig(envPath);
+        
+        // Create and start file monitor
+        FileMonitor monitor(config);
+        monitor.monitor();
+        
+        return 0;
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Fatal error: " << e.what() << std::endl;
+        return 1;
+    }
 }
