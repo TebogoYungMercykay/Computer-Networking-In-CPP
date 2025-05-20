aacd2df6fa0869aa0ebb31c7c6d883d1c666e6a0
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2025-05-20 04:02:42 +0200

    (ftp) Updated the File Monitor Header File

--- src/ftp-server/FileMonitor.h
+++ src/ftp-server/FileMonitor.h
@@ -2,22 +2,24 @@
 #define FILE_MONITOR_H
 
 #include "Config.h"
+#include "FtpClient.h"
 #include <filesystem>
 
 // File monitoring class
 class FileMonitor {
-private:
-    Config config;
-    std::filesystem::file_time_type lastModified;
-    
-    // Get last modification time of a file
-    std::filesystem::file_time_type getFileModificationTime(const std::string& filepath);
-    
-public:
-    FileMonitor(const Config& config);
-    
-    // Monitor file for changes
-    void monitor();
+    private:
+        Config config;
+        std::filesystem::file_time_type lastModified;
+        
+        // Public methods
+        
+        std::filesystem::file_time_type getFileModificationTime(const std::string& filepath);
+        bool uploadFile();
+        void createDirectoryStructure(FtpClient& client, const std::string& remotePath);
+        void monitorWithPolling();
+    public:
+        FileMonitor(const Config& config);
+        void monitor();
 };
 
 #endif // FILE_MONITOR_H
