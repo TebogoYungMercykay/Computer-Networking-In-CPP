98585d4e3baf555e6a53746d27aade173a84c73a
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2025-05-20 04:04:07 +0200

    (ftp) The Updated Main Program Code

--- src/ftp-server/main.cpp
+++ src/ftp-server/main.cpp
@@ -7,16 +7,13 @@ int main(int argc, char* argv[]) {
     try {
         std::string envPath = ".env";
         
-        // Allow specifying a different .env file via command line
         if (argc > 1) {
             envPath = argv[1];
         }
         
-        // Load configuration
         Config config = loadConfig(envPath);
-        
-        // Create and start file monitor
         FileMonitor monitor(config);
+
         monitor.monitor();
         
         return 0;
